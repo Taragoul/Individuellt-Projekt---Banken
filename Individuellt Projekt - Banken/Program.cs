4f68a6aad0dbd7b70874a42a9174418f97b1b0da
Zak Troy <zak.troy@chasacademy.se>
2023-01-10 14:56:35 +0100

    changed added pins to the dictionary, changed the ints to doubles to allow decimals for our ÖRE

--- Individuellt Projekt - Banken/Program.cs
+++ Individuellt Projekt - Banken/Program.cs
@@ -12,14 +12,13 @@ namespace Atm
             Console.WriteLine("Welcome to the bank!");
 
             // Create a dictionary to store the users and their accounts
-            var users = new Dictionary<string, List<Account>> //TODO these also need t obe doubles somehow i would assume...
-            //todo i forgot to use array and will be swapping this to use arrays instead of list!
+            var users = new Dictionary<string, Account[]>
             {
-                { "user1", new List<Account> { new Account("salary", 10000), new Account("savings", 50000) } },
-                { "user2", new List<Account> { new Account("salary", 20000), new Account("savings", 30000) } },
-                { "user3", new List<Account> { new Account("salary", 40000), new Account("savings", 60000) } },
-                { "user4", new List<Account> { new Account("salary", 20000), new Account("savings", 100000) } },
-                { "user5", new List<Account> { new Account("salary", 5000), new Account("savings", 7000) } }
+                { "user1", new Account[] { new Account("salary", 10000.0, "1234"), new Account("savings", 60000.0, "1234") } },
+                { "user2", new Account[] { new Account("salary", 20000.0, "5678"), new Account("savings", 70000.0, "5678") } },
+                { "user3", new Account[] { new Account("salary", 30000.0, "9101"), new Account("savings", 80000.0, "9101") } },
+                { "user4", new Account[] { new Account("salary", 40000.0, "1121"), new Account("savings", 90000.0, "1121") } },
+                { "user5", new Account[] { new Account("salary", 50000.0, "3141"), new Account("savings", 10000.0, "3141") } }
             };
 
             // Keep asking for user input until the user logs out
@@ -41,7 +40,7 @@ namespace Atm
                         Console.WriteLine("1. See your accounts and balance");
                         Console.WriteLine("2. Transfer between accounts");
                         Console.WriteLine("3. Withdraw money");
-                        Console.WriteLine("4. Log out");                   //TODO its not actually logging me out :( just starting the menu over 
+                        Console.WriteLine("4. Log out");
 
                         // Get the user's choice
                         Console.Write("Enter a number: ");
@@ -60,20 +59,20 @@ namespace Atm
                                 break;
                             case "2":
                                 // Transfer money between accounts
-                                Console.WriteLine("Select an account to transfer from:"); //TODO gotta swap from int to possibly doubles? 
-                                for (int i = 0; i < users[userNumber].Count; i++)
+                                Console.WriteLine("Select an account to transfer from:");
+                                for (int i = 0; i < users[userNumber].Length; i++)
                                 {
                                     Console.WriteLine($"{i + 1}. {users[userNumber][i].Name} ({users[userNumber][i].Balance})");
                                 }
                                 int from = int.Parse(Console.ReadLine()) - 1;
                                 Console.WriteLine("Select an account to transfer to:");
-                                for (int i = 0; i < users[userNumber].Count; i++)
+                                for (int i = 0; i < users[userNumber].Length; i++)
                                 {
                                     Console.WriteLine($"{i + 1}. {users[userNumber][i].Name} ({users[userNumber][i].Balance})");
                                 }
                                 int to = int.Parse(Console.ReadLine()) - 1;
                                 Console.Write("Enter the amount to transfer: ");
-                                int amount = int.Parse(Console.ReadLine());
+                                double amount = double.Parse(Console.ReadLine());
                                 if (users[userNumber][from].Balance >= amount)
                                 {
                                     users[userNumber][from].Balance -= amount;
@@ -88,13 +87,13 @@ namespace Atm
                             case "3":
                                 // Withdraw money
                                 Console.WriteLine("Select an account to withdraw from:");
-                                for (int i = 0; i < users[userNumber].Count; i++)
+                                for (int i = 0; i < users[userNumber].Length; i++)
                                 {
                                     Console.WriteLine($"{i + 1}. {users[userNumber][i].Name} ({users[userNumber][i].Balance})");
                                 }
                                 int withdrawFrom = int.Parse(Console.ReadLine()) - 1;
                                 Console.Write("Enter the amount to withdraw: ");
-                                int withdrawAmount = int.Parse(Console.ReadLine()); //TODO ive remembered being told in class that these cant hold Öre as in decimals so i gotta fix it 
+                                double withdrawAmount = double.Parse(Console.ReadLine()) - 1;
                                 if (users[userNumber][withdrawFrom].Balance >= withdrawAmount)
                                 {
                                     Console.Write("Enter your pin to confirm: ");
@@ -116,7 +115,7 @@ namespace Atm
                                 break;
                             case "4":
                                 // Log out
-                                Console.WriteLine("You have been logged out."); //TODO fix this part 
+                                Console.WriteLine("You have been logged out.");
                                 break;
                             default:
                                 // Invalid selection
@@ -125,7 +124,7 @@ namespace Atm
                         }
 
                         // Wait for the user to press enter before showing the main menu again
-                        Console.WriteLine("Press enter to go back to the main menu."); //TODO slightly buggy 
+                        Console.WriteLine("Press enter to go back to the main menu.");
                         Console.ReadLine();
                     }
                 }
@@ -138,8 +137,9 @@ namespace Atm
         }
     }
 
-    // Account class to represent an account
-    class Account
+
+// Account class to represent an account
+class Account
     {
         public string Name { get; set; }
         public double Balance { get; set; }
